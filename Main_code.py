611ee4fe41d36a42692240cc3916e0a5d6d1983b
Zexus <shrestharawat75@gmail.com>
2024-10-20 16:13:14 +0530

    Your commit message

--- Main_code.py
+++ Main_code.py
@@ -6,17 +6,16 @@ def fun(a):
     while in_numb != chosen_numb:
         try:
             in_numb = int(input("Please! enter a  number: "))        
-        
+            if a >= in_numb>= chosen_numb:
+                print("Number is greater")
+            elif 1 <= in_numb <= chosen_numb:
+                print("Number is smaller")
+            elif 1> in_numb or in_numb > a:
+                print(f"Please! enter a number between 1 and {a}") 
         except ValueError: 
             print("Please! Enter a number.")
-        
-        if a >= in_numb>= chosen_numb:
-            print("Number is greater")
-        elif 1 <= in_numb <= chosen_numb: 
-            print("Number is smaller")
-        elif 1> in_numb or in_numb > a:
-            print(f"Please! enter a number between 1 and {a}") 
-    
+
+            
     print(f"Congratulations! Your number {chosen_numb} is correct")
 
 
